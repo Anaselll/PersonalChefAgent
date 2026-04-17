f5688ef1c33fb6c36edf6f57f08fb3de15230613
AnasEll <anaselhaouss89@gmail.com>
2026-04-17 17:18:58 +0100

    add a short term memory

--- app/agent/engine.py
+++ app/agent/engine.py
@@ -4,21 +4,29 @@ from langchain.agents import create_agent
 from app.tools.web_search import web_search
 from langchain.messages import HumanMessage
 from app.model.groq import model
-
+from langgraph.checkpoint.memory import InMemorySaver
 
 
 
 agent=create_agent(
     model=model,
     tools=[web_search],
-    system_prompt="you are a personal chef that cook the best food based on ingredients"
+    system_prompt="you are a personal chef that cook the best food based on ingredients",
+    checkpointer=InMemorySaver()
 )
 
-
+config={"configurable":{"thread_id":"1"}}
 
 response=agent.invoke({
-    "messages":[HumanMessage("i have tomato pasta and chicken")]
-})
+    "messages":[HumanMessage(content="i have tomato pasta and chicken")],
+  
+},  config)
+
+response=agent.invoke({
+    "messages":[HumanMessage(content="detail this recipe")],
+  
+},  config)
 
 
-print(response["messages"][-1].content)
+
+print(response["messages"])
--- app/model/groq.py
+++ app/model/groq.py
@@ -2,4 +2,5 @@ from langchain_groq import ChatGroq
 from app.core.config import GROQ_API_KEY
 model = ChatGroq(
     model="qwen/qwen3-32b",
+   
 )
